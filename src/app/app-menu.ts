7161d0f419e8a0f92f85b941912e77f0ea0ca25b
Makhloufhleli <makhlouf.hleli@gmail.com>
2021-02-14 17:59:59 +0100

    create layout components (header and footer and sidebar), install nebular template and override the style source to nebular and create user components(list component, create component and add menu to the side bar)

---
+++ src/app/app-menu.ts
@@ -0,0 +1,19 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+    {
+      title: 'Home',
+      icon: 'home-outline',
+      link: '/home',
+    },
+    /*{
+      title: 'Classroom',
+      icon: 'book-outline',
+      link: '/classroom',
+    },
+    {
+      title: 'My team',
+      icon: 'people-outline',
+      link: '/team',
+    }*/
+  ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { 
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbMenuModule,
+  NbIconModule,
+  NbActionsModule,
+  NbSearchModule, 
+} from '@nebular/theme';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { AddUserComponent } from './components/user/addUser/add-user.component';
+import { UserListComponent } from './components/user/userList/user-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddUserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbIconModule,
+    NbActionsModule,
+    NbSearchModule,
+    NbEvaIconsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
