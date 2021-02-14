836fd15cf325bb2036fc4ea48b4f2fd083cda7e4
Makhloufhleli <makhlouf.hleli@gmail.com>
2021-02-14 20:13:50 +0100

    custemize the footer section

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">
+      &copy; Created with ♥ by <b><a href="" target="_blank">Makhlouf Helali</a></b> 2021
+    </span>
+    <div class="socials">
+      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+    </div>
+  `,
 })
 export class FooterComponent implements OnInit {
 
