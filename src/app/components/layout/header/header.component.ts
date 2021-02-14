b916f514dbbf324d293702d973ef0b7c440918d9
Makhloufhleli <makhlouf.hleli@gmail.com>
2021-02-14 20:09:08 +0100

    add logo and menu icon to header and search, notifications and messages icons to header section

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { NbSidebarService, NbMenuService } from '@nebular/theme';
+import { LayoutService } from '../../../services/layout/layout.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+
+    private sidebarService: NbSidebarService,
+    private layoutService: LayoutService,
+    private menuService: NbMenuService,
+
+  ) { }
+
+  private destroy$: Subject<void> = new Subject<void>();
+  userPictureOnly: boolean = false;
+  user: any;
+
+  userMenu = [ { title: 'Profile' }, { title: 'Log out' } ];
+  
 
   ngOnInit(): void {
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.layoutService.changeLayoutSize();
+
+    return false;
+  }
+
+  navigateHome() {
+    this.menuService.navigateHome();
+    return false;
   }
 
 }
